68bb046b09f28fa497d42aa425180083276b4009
zheng <zhen.wang@target-energysolutions.com>
2018-07-09 15:43:42 +0800

    :sparkles: [feat] the menu main framework

--- src/components/wechat-menu/index.js
+++ src/components/wechat-menu/index.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react'
+import avatar from '/Users/zheng/Desktop/myproject/wechat/src/imags/me.jpg'
 import './style.css'
 
 export default class WechatMenu extends Component {
@@ -6,7 +7,12 @@ export default class WechatMenu extends Component {
    render() {
        return (
            <div className="wechat-menu"> 
-           menu
+           <div>control bar</div>
+           <div>
+            <img srcSet={avatar} alt="avatar" className="wechat-menu-avatar"/>
+            <div>menu switch</div>
+            </div>
+            <div>menu more</div>
            </div>
        );
    }
